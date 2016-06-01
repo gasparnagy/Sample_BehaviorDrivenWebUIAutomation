0283e8ce01bea1cfdbbeb800a058ffbdcf851b1e
Gaspar Nagy <gaspar.nagy@gmail.com>
2016-06-01 12:36:04 +0200

    Implemented ContainerConcept

---
+++ BehaviorDrivenWebUIAutomation.SpecFlowTests/Concepts/ContainerConcept.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace BehaviorDrivenWebUIAutomation.SpecFlowTests.Concepts
+{
+    public interface IUiContainerConcept
+    {
+        ISearchContext SearchContext { get; }
+    }
+
+    public abstract class ContainerConcept : IUiContainerConcept
+    {
+        private class LazyElementLocator : IElementLocator
+        {
+            private readonly Lazy<ISearchContext> searchContext;
+
+            public ISearchContext SearchContext { get { return searchContext.Value; } }
+
+            public LazyElementLocator(Lazy<ISearchContext> searchContext)
+            {
+                this.searchContext = searchContext;
+            }
+
+            public IWebElement LocateElement(IEnumerable<By> bys)
+            {
+                return new DefaultElementLocator(SearchContext).LocateElement(bys);
+            }
+
+            public ReadOnlyCollection<IWebElement> LocateElements(IEnumerable<By> bys)
+            {
+                return new DefaultElementLocator(SearchContext).LocateElements(bys);
+            }
+        }
+
+        private readonly Lazy<ISearchContext> searchContext;
+
+        protected ContainerConcept(string key, IElementLocator elementLocator)
+        {
+            searchContext = new Lazy<ISearchContext>(() => elementLocator.LocateElement(LocatorConventions.GetLocators(key)));
+            ConceptFactory.InitElements(new LazyElementLocator(searchContext), this);
+        }
+
+        protected ContainerConcept(ISearchContext searchContext)
+        {
+            this.searchContext = new Lazy<ISearchContext>(() => searchContext);
+            ConceptFactory.InitElements(searchContext, this);
+        }
+
+        public ISearchContext SearchContext { get { return searchContext.Value; } }
+    }
+}
---
+++ BehaviorDrivenWebUIAutomation.SpecFlowTests/Concepts/Page.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+
+namespace BehaviorDrivenWebUIAutomation.SpecFlowTests.Concepts
+{
+    public abstract class Page : ContainerConcept
+    {
+        protected Page(IWebDriver webDriver) : base(webDriver)
+        {
+        }
+    }
+}
